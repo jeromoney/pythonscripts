678bd332aab8ae3647617c662c733aab9551e588
Jermoney <justin.matis.com@gmail.com>
2015-07-02 09:04:42 -0600

    Start from blank file

--- race.py
+++ race.py
@@ -1,75 +1,28 @@
 import math
-from math import cos, sin
 import numpy as np
 from plot import plot, plot_trajectory, plot_covariance_2d
 
-
-
 class UserCode:
     def __init__(self):
-        #PID parameters
-        Kp_xy = 2
-        Kp_z = 1
-        Kd_xy = 1
-        Kd_z = 0
-
-        self.Kp = np.array([[Kp_xy, Kp_xy, Kp_z]]).T
-        self.Kd = np.array([[Kd_xy, Kd_xy, Kd_z]]).T
-        #process noise
-        pos_noise_std = 0.005
-        yaw_noise_std = 0.005
-        self.Q = np.array([
-            [pos_noise_std*pos_noise_std,0,0],
-            [0,pos_noise_std*pos_noise_std,0],
-            [0,0,yaw_noise_std*yaw_noise_std]
-        ])
-
-        #measurement noise
-        z_pos_noise_std = 0.005
-        z_yaw_noise_std = 0.03
-        self.R = np.array([
-            [z_pos_noise_std*z_pos_noise_std,0,0],
-            [0,z_pos_noise_std*z_pos_noise_std,0],
-            [0,0,z_yaw_noise_std*z_yaw_noise_std]
-        ])
-
-        # state vector [x, y, yaw] in world coordinates
-        self.x = np.zeros((3,1))
-        self.x_desired = np.array([0,0,1])
-        self.velocity = np.zeros((3,1))
-
-        # 3x3 state covariance matrix
-        self.sigma = 0.01 * np.identity(3)
-
+        pass
+        
     def get_markers(self):
         '''
         place up to 30 markers in the world
         '''
         markers = [
-             [-1, -1],
-             [-1,0],
-             [1,0],
-             [0,1],
-             [0,-1],
-             [-1,1],
-             [1,-1],
              [0, 0], # marker at world position x = 0, y = 0
-             [2, 0],  # marker at world position x = 2, y = 0
-             [1, 1],
-             [2, 2],
-             [3, 3],
-             [4, 4],
-             [5, 5]
+             [2, 0]  # marker at world position x = 2, y = 0
         ]
-
+        
         #TODO: Add your markers where needed
-
+       
         return markers
-
+        
     def state_callback(self, t, dt, linear_velocity, yaw_velocity):
         '''
         called when a new odometry measurement arrives approx. 200Hz
-
+    
         :param t - simulation time
         :param dt - time difference this last invocation
         :param linear_velocity - x and y velocity in local quadrotor coordinate frame (independet of roll and pitch)
@@ -77,205 +30,19 @@ class UserCode:
 
         :return tuple containing linear x and y velocity control commands in local quadrotor coordinate frame (independet of roll and pitch), and yaw velocity
         '''
-        self.x = self.predictState(dt, self.x, linear_velocity, yaw_velocity)
-
-        F = self.calculatePredictStateJacobian(dt, self.x, linear_velocity, yaw_velocity)
-        self.sigma = self.predictCovariance(self.sigma, F, self.Q);
-        self.velocity = self.velocity_inversion(linear_velocity,yaw_velocity)
-        return self.compute_control_command(),0.
-    def velocity_inversion(self,linear_velocity,yaw_velocity):
-        '''
-        called to invert a local world velocity to an absolute world velocity
-        :param linear_velocity - x,y velocity in local world
-        :param yaw_velocity - turning velocity in local world
-        :return 3-d velocity in absolute coordinates:
-        '''
-        rotation = self.rotation(self.x[2])
-        invRotation = np.linalg.inv(rotation)
-        velocity = np.dot(invRotation,linear_velocity)
-        velocity = np.concatenate((velocity,np.zeros(1)))
-        return velocity
+        
+        return np.ones((2,1)) * 0.1, 0
 
 
     def measurement_callback(self, marker_position_world, marker_yaw_world, marker_position_relative, marker_yaw_relative):
         '''
         called when a new marker measurement arrives max 30Hz, marker measurements are only available if the quadrotor is
         sufficiently close to a marker
-
+            
         :param marker_position_world - x and y position of the marker in world coordinates 2x1 vector
         :param marker_yaw_world - orientation of the marker in world coordinates
         :param marker_position_relative - x and y position of the marker relative to the quadrotor 2x1 vector
         :param marker_yaw_relative - orientation of the marker relative to the quadrotor
         '''
-        z = np.array([[marker_position_relative[0], marker_position_relative[1], marker_yaw_relative]]).T
-        z_predicted = self.predictMeasurement(self.x, marker_position_world, marker_yaw_world)
-
-        H = self.calculatePredictMeasurementJacobian(self.x, marker_position_world, marker_yaw_world)
-        K = self.calculateKalmanGain(self.sigma, H, self.R)
-
-        self.x = self.correctState(K, self.x, z, z_predicted)
-        self.sigma = self.correctCovariance(self.sigma, K, H)
-
-    def compute_control_command(self):
-        #I've set the desired velocity to 0. Optimally the desired velocity should be pointing towards the next marker
-        p = self.Kp * (self.x_desired - self.x)
-        d = self.Kd * (np.zeros((3,1)) - self.velocity)
-        u =  p + d
-        return u[:1]
-
-    def rotation(self, yaw):
-        '''
-        create 2D rotation matrix from given angle
-        '''
-        s_yaw = math.sin(yaw)
-        c_yaw = math.cos(yaw)
-
-        return np.array([
-            [c_yaw, -s_yaw],
-            [s_yaw,  c_yaw]
-        ])
-
-    def normalizeYaw(self, y):
-        '''
-        normalizes the given angle to the interval [-pi, +pi]
-        '''
-        while(y > math.pi):
-            y -= 2 * math.pi
-        while(y < -math.pi):
-            y += 2 * math.pi
-        return y
-
-    def visualizeState(self):
-        # visualize position state
-        plot_trajectory("kalman", self.x[0:2])
-        plot_covariance_2d("kalman", self.sigma[0:2,0:2])
-
-    def predictState(self, dt, x, u_linear_velocity, u_yaw_velocity):
-        '''
-        predicts the next state using the current state and
-        the control inputs local linear velocity and yaw velocity
-        '''
-        x_p = np.zeros((3, 1))
-        x_p[0:2] = x[0:2] + dt * np.dot(self.rotation(x[2]), u_linear_velocity)
-        x_p[2]   = x[2]   + dt * u_yaw_velocity
-        x_p[2]   = self.normalizeYaw(x_p[2])
-
-        return x_p
-
-    def calculatePredictStateJacobian(self, dt, x, u_linear_velocity, u_yaw_velocity):
-        '''
-        calculates the 3x3 Jacobian matrix for the predictState(...) function
-        '''
-        s_yaw = math.sin(x[2])
-        c_yaw = math.cos(x[2])
-
-        dRotation_dYaw = np.array([
-            [-s_yaw, -c_yaw],
-            [ c_yaw, -s_yaw]
-        ])
-        F = np.identity(3)
-        F[0:2, 2] = dt * np.dot(dRotation_dYaw, u_linear_velocity)
-
-        return F
-
-    def predictCovariance(self, sigma, F, Q):
-        '''
-        predicts the next state covariance given the current covariance,
-        the Jacobian of the predictState(...) function F and the process noise Q
-        '''
-        return np.dot(F, np.dot(sigma, F.T)) + Q
-
-    def calculateKalmanGain(self, sigma_p, H, R):
-        '''
-        calculates the Kalman gain
-        '''
-        return np.dot(np.dot(sigma_p, H.T), np.linalg.inv(np.dot(H, np.dot(sigma_p, H.T)) + R))
-
-    def correctState(self, K, x_predicted, z, z_predicted):
-        '''
-        corrects the current state prediction using Kalman gain, the measurement and the predicted measurement
-
-        :param K - Kalman gain
-        :param x_predicted - predicted state 3x1 vector
-        :param z - measurement 3x1 vector
-        :param z_predicted - predicted measurement 3x1 vector
-        :return corrected state as 3x1 vector
-        '''
-        residual = (z - z_predicted)
-        residual[2] = self.normalizeYaw(residual[2])
-
-        return x_predicted + np.dot(K, residual)
-
-    def correctCovariance(self, sigma_p, K, H):
-        '''
-        corrects the sate covariance matrix using Kalman gain and the Jacobian matrix of the predictMeasurement(...) function
-        '''
-        return np.dot(np.identity(3) - np.dot(K, H), sigma_p)
-
-    def predictMeasurement(self, x, marker_position_world, marker_yaw_world):
-        '''
-        predicts a marker measurement given the current state and the marker position and orientation in world coordinates
-        '''
-        z_predicted = Pose2D(self.rotation(x[2]), x[0:2]).inv() * Pose2D(self.rotation(marker_yaw_world), marker_position_world);
-
-        return np.array([[z_predicted.translation[0], z_predicted.translation[1], z_predicted.yaw()]]).T
-
-    def calculatePredictMeasurementJacobian(self, x, marker_position_world, marker_yaw_world):
-        '''
-        calculates the 3x3 Jacobian matrix of the predictMeasurement(...) function using the current state and
-        the marker position and orientation in world coordinates
-
-        :param x - current state 3x1 vector
-        :param marker_position_world - x and y position of the marker in world coordinates 2x1 vector
-        :param marker_yaw_world - orientation of the marker in world coordinates
-        :return - 3x3 Jacobian matrix of the predictMeasurement(...) function
-        '''
-        s_yaw = math.sin(x[2])
-        c_yaw = math.cos(x[2])
-
-        dx = marker_position_world[0] - x[0];
-        dy = marker_position_world[1] - x[1];
-
-        return np.array([
-            [-c_yaw, -s_yaw, -s_yaw * dx + c_yaw * dy],
-            [ s_yaw, -c_yaw, -c_yaw * dx - s_yaw * dy],
-            [     0,      0,                      -1]])
-
-
-class Pose2D:
-    def __init__(self, rotation, translation):
-        self.rotation = rotation
-        self.translation = translation
-
-    def inv(self):
-        '''
-        inversion of this Pose2D object
-
-        :return - inverse of self
-        '''
-        inv_rotation = self.rotation.transpose()
-        inv_translation = -np.dot(inv_rotation, self.translation)
-
-        return Pose2D(inv_rotation, inv_translation)
-
-    def yaw(self):
-        from math import atan2
-        return atan2(self.rotation[1,0], self.rotation[0,0])
-
-    def __mul__(self, other):
-        '''
-        multiplication of two Pose2D objects, e.g.:
-            a = Pose2D(...) # = self
-            b = Pose2D(...) # = other
-            c = a * b       # = return value
-
-        :param other - Pose2D right hand side
-        :return - product of self and other
-        '''
-        return Pose2D(np.dot(self.rotation, other.rotation), np.dot(self.rotation, other.translation) + self.translation)
-
-class State:
-    def __init__(self):
-        self.position = np.zeros((3,1))
-        self.velocity = np.zeros((3,1))
-
+        pass
+    
